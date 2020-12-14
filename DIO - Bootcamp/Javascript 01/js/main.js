5be805af259d424bb01745a2160b086d0494b2e9
Dannilo Carlos <dannilocarlos@gmail.com>
2020-12-13 21:47:44 -0300

    13/12

---
+++ DIO - Bootcamp/Javascript 01/js/main.js
@@ -0,0 +1,77 @@
+function botao(){
+  document.getElementById("agd").innerHTML = "Obrigado por clicar";
+}
+
+function redirecionar(){
+  window.open("https://www.google.com")
+}
+
+/*
+function soma(n1, n2) {
+  return n1 + n2;
+}
+
+let idade = prompt("a")
+console.log(validaIdade(idade))
+
+function validaIdade(idade){
+  let validar;
+  if(idade >= 18){
+    validar = true;
+  }else{
+    validar = false;
+  }
+  return validar;
+}
+
+let d = new Date();
+alert(d.getMinutes());
+ // getHours, getDay, GetMinutes...
+
+
+let count;
+for(count=0; count <= 5; count++)
+alert(count);
+
+
+let count = 0;
+while (count <= 5) {
+  console.log(count);
+  count++;
+};
+
+
+let idade = prompt("Idade:");
+if (idade >= 18){
+  alert("maior de idade");
+}else{
+  alert("menor de idade");
+}
+
+//let frutas = [{nome: "maça", cor:"vermelha"}, {nome: "uva", cor:"verde"}]
+//console.log(frutas[1].nome);
+
+
+let lista = ["01", "02", "03", "04", "05"];
+console.log(lista[1]);
+console.log("---------------");
+lista.push("06");
+console.log(lista);
+console.log("---------------");
+lista.pop();
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" "));
+
+--------------------------
+
+let nome = "Dannilo Carlos";
+let n1 = 5;
+let n2 = 3;
+let frase = "Brasil, Brasil irra";
+// alert("Nome: " + nome + " | Idade: " + idade + " anos");
+// alert(idade + idade2)
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.replace("Brasil", "BRASILLLL"));
+console.log(frase.toUpperCase()); */
